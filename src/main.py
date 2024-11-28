cc07f7b70f7b152f56c03d56a9caae85439b372e
Louis <836250617@qq.com>
2024-11-29 01:38:36 +0800

    fix: correct state

--- src/main.py
+++ src/main.py
@@ -1,19 +1,17 @@
 from pprint import pprint as print
-
+from random import uniform, choice
 
 class QLearning:
     def __init__(self):
-        self._q_table: dict[str, dict[str, float]] = {
-            "waiting": {"up": 0, "down": 0},
-            "fighting": {"up": 0, "down": 0},
-            "final": {"up": 0, "down": 0},
-        }
+        self._people = 25
+        self._q_table: dict[int, dict[str, float]] = dict((
+            (i, {"up": 0, "down": 0}) for i in range(self._people + 1)
+        ))
         self._learning_rate = 0.1
         self._discount_factor = 0.9
         self._state = "waiting"
         self._next_state = "fighting"
         self._time = 0
-        self._people = 25
         self._process = 0
 
     @property
@@ -24,13 +22,20 @@ class QLearning:
         """
         Get the max q_value of the next state
         """
-        return max(self._q_table[self._next_state].values())
+        return max(self._q_table[self._people].values())
 
     def _select_action(self) -> str:
-        return max(
-            self._q_table[self._state].keys(),
-            key=lambda x: self._q_table[self._state][x],
-        )
+        floor = min(self._q_table[self._people].values())
+        total = sum((v - floor for v in self._q_table[self._people].values()))
+        if all((v - floor == 0 for v in self._q_table[self._people].values())):
+            return choice(list(self._q_table[self._people].keys()))
+        rand = uniform(0, total)
+        tmp = 0
+        for (k, v) in self._q_table[self._people].items():
+            if tmp + v - floor > rand:
+                return k
+            tmp += v - floor
+        raise RuntimeError("Unreachable code reached")
 
     def _step(self, action: str):
         self._time += 1
@@ -53,14 +58,14 @@ class QLearning:
 
         # update people
         if self._state == "waiting":
-            self._people = max(self._people - 5, 0)
+            self._people = max(self._people - 8, 0)
         elif self._state == "fighting":
-            self._people = max(self._people - 1, 0)
+            self._people = max(self._people - 2, 0)
 
         r = -self._time
 
-        self._q_table[self._state][action] = (1 - self._learning_rate) * self._q_table[
-            self._state
+        self._q_table[self._people][action] = (1 - self._learning_rate) * self._q_table[
+            self._people
         ][action] + self._learning_rate * (r + self._discount_factor * self._max_q())
 
         self._state = self._next_state
